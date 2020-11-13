94f550cc43928323fe8f186f9ecdc698082579bb
omur8819 <omur8819@gmail.com>
2020-11-13 16:55:38 +0300

    useState added

--- loginPageDropbox/App.js
+++ loginPageDropbox/App.js
@@ -1,114 +1,146 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, 
+        Image, 
+        View, 
+        StyleSheet, 
+        TextInput, 
+        Text, 
+        TouchableOpacity,
+        Alert} from 'react-native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const App = (props) => {
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const [email, setEmail] = useState("");
 
-const App: () => React$Node = () => {
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    Alert.alert("CLARUSWAY", `E-mail: ${email}, Password: ${password}`)
+  }
+
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={{flex:1}}>
+      <View style={styles.container}>
+        
+        <View style={styles.image}>
+          <Image 
+            source={require("./src/dropbox-font.png")} 
+            style={{width:300, height:150,}}
+          />
+        </View>
+
+        <View>
+          <TextInput 
+            value={email}
+            style={styles.input} 
+            placeholder='Username or e-mail'
+            onChangeText={(userText) => setEmail(userText)} 
+            keyboardType='email-address'
+          />
+          
+          <TextInput 
+            value={password}
+            style={styles.input} 
+            placeholder='Password'
+            onChangeText={(userPass) => setPassword(userPass)} 
+            keyboardType='password'
+            secureTextEntry={true}
+
+          />
+
+          <Text style={styles.password}>Forgot password?</Text>
+
+          <TouchableOpacity style={styles.button} onPress={login}>
+            <Text style={{color: 'white', fontSize: 16,}}>Log In</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+            style={styles.button} 
+            onPress={() => {
+              setEmail("");
+              setPassword("");
+            }}
+          >
+            <Text style={{color: 'white', fontSize: 16,}}>Clear</Text>
+          </TouchableOpacity>
+
+          <Text style={{color: 'black', alignSelf: 'center', padding: 10, fontWeight: 'bold'}}>OR</Text>
+
+          <Text style={styles.google}>
+
+            <Image 
+              source={require("./src/google-favicon.png")} 
+              style={{width:3, height:3}}
+            />
+            Sign up with Google
+          </Text>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={{color: 'white', fontSize: 16,}}>Create New Account</Text>
+          </TouchableOpacity>
+
+          <Image 
+            source={require("./src/dropbox-icon2.png")} 
+            style={{width:150, height:150, marginLeft: 125, marginTop: 30,}}/>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+const styles=StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgb(0, 126, 229)',
+    zIndex: 2,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+
+  image: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+
   },
-  body: {
-    backgroundColor: Colors.white,
+  input: {
+    padding: 8,
+    margin: 10,
+    marginRight: 30,
+    marginLeft: 30,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    fontSize: 16,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  button: {
+    alignItems: "center",
+    backgroundColor: "#0D2481",
+    padding: 10,
+    marginLeft: 40,
+    marginRight: 40,
+    marginBottom: 10,
+    marginTop:20,
+    borderRadius: 10,
+    fontSize: 14,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  password: {
+    color: 'white', 
+    alignSelf: 'flex-end', 
+    padding: 2, 
+    margin: 10,
+    textDecorationLine: 'underline',
+    marginRight: 30,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+
+   google: {
+     fontSize: 18,
+     color: 'white', 
+     alignSelf: 'center', 
+     padding: 5, 
+     
+   }
+  
+})
 
 export default App;
